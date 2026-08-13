4b9f178ec1481a4903d10bf928f9779e87689cd2
sahadathossain33331-sketch <sahadathossain33331@gmail.com>
2026-08-13 16:23:38 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:cronet_http/cronet_http.dart';
 
 const supabaseUrl = 'https://vleaqmiyihlginevgfmw.supabase.co';
 const supabasePublishableKey =
